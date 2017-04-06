e709e71c0cb84d97920bceb21bb0309bef38b239
Sergei Paderov <sergeipaderov@gmail.com>
2017-04-06 20:43:23 -0300

    task´s comment

--- learnyounode/lesson3.js
+++ learnyounode/lesson3.js
@@ -1,3 +1,21 @@
+/*  file://C:\Users\serge\AppData\Roaming\npm\node_modules\learnyounode\node_a
+  pidoc\fs.html
+
+  Объекты типа Buffer используются Node для эффективного представления
+  массивов данных, будь это ascii, бинарный или любой другой формат. Эти
+  объекты могут быть конвертированы в строки путем вызовом метода toString()
+  над ними, например var str = buf.toString().
+
+  Документацию для Buffer вы сможете найти набрав в браузере:
+  file://C:\Users\serge\AppData\Roaming\npm\node_modules\learnyounode\node_a
+  pidoc\buffer.html
+
+  Если Вы ищите простой способ посчитать строк в строке, то Вы можете
+  использовать .split() с разделителем \n для разбиения этой строки на
+  массив подстрок. Примите во внимание то, что тестовый файл не содержит
+  символ \n в конце последней строки, таки образом у вас получится массив
+  который содержит на один элемент больше чем число строк в файле.*/
+
 const fs = require('fs');
 
 const buf = fs.readFile(process.argv[2], function(err, buf) {
